e0215b4920ac0bb6c686326f07ad326e17e728cb
David Evans <david.evans@blackpoint.co>
2019-11-26 23:37:50 +0000

    Implement redirect service

--- Sources/3wurlRedirect/main.swift
+++ Sources/3wurlRedirect/main.swift
@@ -5,6 +5,9 @@
 //  Created by David Evans on 25/11/2019.
 //
 
-import Foundation
+import Jupiter
 
-print("hello")
+let ports = ListenerPorts.HTTP(9986)
+let config = CoreConfiguration(logLevel: .debug, listenerPorts: ports)
+let server = RedirectServer()
+server.run(configuration: config)
