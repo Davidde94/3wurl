71fad57d778e6658de94d9a7009549fad53ba041
David Evans <david.evans@blackpoint.co>
2020-02-22 22:46:38 +0000

    Fix deploy (#27)

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-source ~/.bashrc
+# setup the path
+PATH="$PATH:/usr/local/bin/swift/usr/bin"
+
+# download the code
 git fetch --all
 git reset --hard origin/master
+
+# compile and run
 swift package resolve
 swift build -c release
 sudo service banterurl restart
