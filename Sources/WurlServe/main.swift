8620aff3d9dcbff3b64871f7389661f7d6d4be82
David Evans <david.evans@blackpoint.co>
2020-03-14 21:53:51 +0000

    Make it all work

--- Sources/WurlServe/main.swift
+++ Sources/WurlServe/main.swift
@@ -23,12 +23,25 @@ struct CreateWurlRequest: Decodable {
     var url: URL
 }
 
-app.on(.POST, "create", body: .collect(maxSize: 256)) { (request: Request) -> EventLoopFuture<Wurl> in
+struct CreateWurlResponse: ResponseEncodable {
+    
+    func encodeResponse(for request: Request) -> EventLoopFuture<Response> {
+        let response = Response()
+        try! response.content.encode(["url":url], as: .json)
+        return response.encodeResponse(status: .created, headers: HTTPHeaders([]), for: request)
+    }
+    
+    var url: URL
+}
+
+app.on(.POST, "create", body: .collect(maxSize: 256)) { (request: Request) -> EventLoopFuture<CreateWurlResponse> in
     guard let data = request.body.data else {
         throw Abort(.badRequest, reason: "Missing JSON payload", suggestedFixes: ["Make sure to send a valid JSON-encoded payload"])
     }
     let decoded = try! JSONDecoder().decode(CreateWurlRequest.self, from: data)
-    return BanterIdentifierManager.createIdentifier(for: decoded.url, on: request.db)
+    return BanterIdentifierManager.createIdentifier(for: decoded.url, on: request.db).map { wurl in
+        return CreateWurlResponse(url: URL(string: "https://3wl.uk/\(wurl.identifier)")!)
+    }
 }
 
 app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
